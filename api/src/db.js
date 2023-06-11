9e2aef0cc8f28fc3db6c384ab629f363c57125ba
sorrus-dev <amr89.dev@gmail.com>
2023-06-10 21:52:28 -0500

    arreglando la base de datos

--- api/src/db.js
+++ api/src/db.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
@@ -7,7 +7,7 @@ const DogModel = require("./models/Dog");
 const TemperamentModel = require("./models/Dog");
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/dogs`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
